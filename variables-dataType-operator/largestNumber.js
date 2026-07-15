10ecebd72e5c33487777653405bb309dbdc67689
Uttam <uttamrohit4545@gmail.com>
2026-07-16 03:45:32 +0600

    feat : largest number problem solve

---
+++ variables-dataType-operator/largestNumber.js
@@ -0,0 +1,22 @@
+function largest(a, b, c) {
+    return Math.max(a, b, c);
+  }
+  
+  // Example
+  console.log(largest(3, 7, 5)); // 7
+
+
+
+
+
+//   function largest(a, b, c) {
+//     if (a >= b && a >= c) {
+//       return a;
+//     } else if (b >= a && b >= c) {
+//       return b;
+//     } else {
+//       return c;
+//     }
+//   }
+  
+//   console.log(largest(3, 7, 5)); // 7
